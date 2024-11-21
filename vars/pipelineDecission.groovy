c37628329bb87e1a91bf81cc9de997a52301803c
rameshkpit1 <42575776+rameshkpit1@users.noreply.github.com>
2024-11-21 13:26:23 +0530

    Update pipelineDecission.groovy

--- vars/pipelineDecission.groovy
+++ vars/pipelineDecission.groovy
@@ -1,4 +1,17 @@
 #!groovy
 def decidePipeline(Map configMap){
+  application = configMap.get("application")
+    switch(application){
+      case 'nodeJSVM'
+        echo "application NodeJS and VM"
+        break
+      case 'javaVM'
+        javaVMCI(configMap)
+        break
+      default:
+        error "unrecognized"
+        break
+      
+    }
   echo " I need to take the decission based on the map you sent"
 }
